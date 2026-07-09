19fc59a8f197882bebfd78a6fac59fc76a42917f
Santhosh Kumar <santhoshsk2323@gmail.com>
2026-07-09 17:39:28 +0530

    Point frontend to production backend URL

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://family-gallery-tiok.onrender.com";
 
 async function apiFetch(path, options = {}) {
   const res = await fetch(`${API_BASE}${path}`, {
